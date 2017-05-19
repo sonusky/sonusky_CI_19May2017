64f490acd97601d7c5204c287d4ba7dbc4a328ea
sonugithubb <sandipskr@gmail.com>
2017-05-19 21:58:20 +0530

    ok

---
+++ Test_CI_1/src/com/sandip/Hello.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.sandip;
+
+/**
+ * @author sandip
+ *
+ */
+public class Hello {
+
+	/**
+	 * 
+	 */
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+		
+		System.out.println("Ok aaa");
+
+	}
+
+}
